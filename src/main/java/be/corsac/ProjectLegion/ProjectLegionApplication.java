8940ea0f412d653cc1682cf43beadf9d411b0d63
GuillaumeLambermont <guillaume.lambermont@gmail.com>
2025-03-05 18:55:51 +0100

    - Initial commit

---
+++ src/main/java/be/corsac/ProjectLegion/ProjectLegionApplication.java
@@ -0,0 +1,13 @@
+package be.corsac.ProjectLegion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLegionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLegionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/corsac/ProjectLegion/ProjectLegionApplicationTests.java
@@ -0,0 +1,13 @@
+package be.corsac.ProjectLegion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLegionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
