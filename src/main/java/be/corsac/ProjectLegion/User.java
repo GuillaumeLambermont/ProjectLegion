a4ac5e538d993c88dcdfd2d2f0ecc62fb3bd6207
GuillaumeLambermont <guillaume.lambermont@gmail.com>
2025-03-06 18:39:46 +0100

    - Initial commit

---
+++ src/main/java/be/corsac/ProjectLegion/User.java
@@ -0,0 +1,37 @@
+package be.corsac.ProjectLegion;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {}
+    public User(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
